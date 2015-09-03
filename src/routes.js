98e485462581c30bde60b77057dfd905f9eecddc
Maxim Lobanov <mlobanov@deloitte.com.au>
2015-09-03 15:53:01 +1000

    Changed anchors into Link tags

--- src/routes.js
+++ src/routes.js
@@ -16,5 +16,30 @@
         </Route>
     );
 
+    /*
+     Given a route like this:
+     <route path="/course/:courseId" handler={ Course } />
+
+     and a URL like this:
+     '/course/clean-code?module=3'
+
+     The component's props will be populated
+     var Course = React.createClass({
+        render: function() {
+            this.props.params.courseId; // "clean-code"
+            this.props.query.module; // "3"
+            this.props.path; // "/course/clean-code/?module=3"
+            // ...
+        }
+     });
+
+     LINKS:
+     URL: /user/1
+     Route: <route name="user" path="/user/:userId" />
+     JSX: <Link to="user" params={{userId: 1}}>Bobby Tables</Link> // references route by name
+        Compiles to: <a href="/user/1">Bobby Tables</a>
+
+    */
+
     module.exports = routes;
 })();
