7f5521403f8ccadc5dc3d246ed78c668e7eeb260
Maxim Lobanov <mlobanov@deloitte.com.au>
2015-09-04 15:44:12 +1000

    Switched to hash location style URLs

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@
     var routes = require('./routes');
 
     // Add in Router.HistoryLocation to use HTML5 history location style (uses push state to change URLs)
-    Router.run(routes, Router.HistoryLocation, function(Handler) {
+    Router.run(routes, function(Handler) {
         React.render(<Handler/>, document.getElementById('app'));
     });
 })();
